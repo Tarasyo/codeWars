0e27119fb400af1023d66f35c3cdc7eda8db790b
Taras Boreyko <tarasyo7@gmail.com>
2024-09-24 16:03:19 +0200

    add BraceChecker kata

---
+++ src/braceChecker/MyBraceChecker.java
@@ -0,0 +1,25 @@
+package braceChecker;
+
+import java.util.Stack;
+
+public class MyBraceChecker {
+    public boolean isValid(String braces) {
+        // Add code here
+        Stack<Character> stk = new Stack<>();
+        for (int i = 0; i < braces.length(); i++) {
+            if (braces.charAt(i) == '(' || braces.charAt(i) == '{' || braces.charAt(i) == '[') {
+                stk.push(braces.charAt(i));
+            } else {
+                if (!stk.empty() &&
+                        ((stk.peek() == '(' && braces.charAt(i) == ')') ||
+                                (stk.peek() == '{' && braces.charAt(i) == '}') ||
+                                (stk.peek() == '[' && braces.charAt(i) == ']'))) {
+                    stk.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stk.empty();
+    }
+}
